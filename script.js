b3d643fe33ed543662940f9a2626906114d60d68
Anukul Raj <85643622+rajanukul@users.noreply.github.com>
2022-08-20 16:58:31 +0530

    final updated

--- script.js
+++ script.js
@@ -88,19 +88,3 @@ function consoleText(words, id, colors) {
 
 
 
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
